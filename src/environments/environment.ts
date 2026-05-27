348ca9d9f2bc691d5ae30da990bcda619ba8e5df
Eze Onykachi Martin <martinmichael1995@gmail.com>
2026-05-27 13:56:53 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://testdogofinance.runasp.net/api'
-  apiUrl: 'https://localhost:7168/api'
-
+  apiUrl: 'http://testdogofinance.runasp.net/api'
 };
