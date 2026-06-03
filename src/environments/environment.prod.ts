ea07ce31b3908aea700772ed347ecc7860c5b181
Eze Onykachi Martin <martinmichael1995@gmail.com>
2026-06-03 15:20:42 +0100

    balance redesigning

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://testdogofinance.runasp.net/api'
+  // apiUrl: 'https://testdogofinance.runasp.net/api'
+  apiUrl: 'https://localhost:7168/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://testdogofinance.runasp.net/api'
-  // apiUrl: 'https://localhost:7168/api'
+  // apiUrl: 'http://testdogofinance.runasp.net/api'
+  apiUrl: 'https://localhost:7168/api'
 };
