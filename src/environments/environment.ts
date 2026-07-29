cc581219e25caa4fe644878cecedc81468edbf51
Malik <malik.s@allprimetech.com>
2026-07-29 15:46:25 +0100

    new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://testdogofinance.runasp.net/api'
-  //  apiUrl: 'https://localhost:7168/api'
+  // apiUrl: 'https://testdogofinance.runasp.net/api'
+   apiUrl: 'https://localhost:7168/api'
 };
